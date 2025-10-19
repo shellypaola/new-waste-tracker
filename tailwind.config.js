165b94507517e328fa61eafea9622df5e6dfca71
shellypaola <shellypvr@gmail.com>
2025-10-20 09:56:39 +1030

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#84CC16',
+      },
+    },
+  },
+  plugins: [],
+}

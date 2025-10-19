05f2c969970570ab55bc9ac85652f4bf2f71656d
shellypaola <shellypvr@gmail.com>
2025-10-20 10:15:47 +1030

    Change primary color and add variants in Tailwind config
    Updated primary color and added light/dark variants.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#84CC16',
+        primary: '#3B82F6',      // Blue-500
+        'primary-dark': '#1D4ED8', // Blue-700
+        'primary-light': '#60A5FA', // Blue-400
       },
     },
   },
